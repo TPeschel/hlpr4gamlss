80363086aaaf566e7e10caed42c2f81bba43259e
TPeschel <TPeschel@life.uni-leipzig.de>
2018-11-26 12:35:09 +0100

    add some nice funcs

--- R/functions.R
+++ R/functions.R
@@ -1,3 +1,5 @@
+library( gamlss )
+
 #' best.match
 #'
 #' @name best.match
@@ -38,6 +40,225 @@ which.best.match <-
 			actual,
 			function( a )
 				which.min( abs( a - nominal ) ) )
+	}
+
+
+#' new.Call
+#' @name new.Call
+#' @description  creates a new.Call object for using with do.Call
+#' @param func function's name as string
+#' @param ...  arguments of the function
+#' @param canonical if TRUE a call with all neccessary arguments is called. Missing will be filled by args of the function's default call.
+#'
+#' @return a new.Call object
+#' @export
+#'
+#' @examples
+#' n.C <- new.Call( "print", "Hello World!" )
+#' do.Call( n.C )
+new.Call <-
+	function( func = "Sys.time", ..., canonical = F ) {
+		args.list <- list( ... )
+		if( canonical ) {
+			frmls           <- formals( func )
+			frmls.names     <- names( frmls )
+			args.list.names <- names( args.list )
+			empty.names     <- c( )
+			if( is.null( args.list.names ) && 0 < length( args.list ) )
+				empty.names <- c( 1 : length( args.list ) )
+			else
+				empty.names <- which( args.list.names == "" )
+
+			common.names                             <- setdiff( frmls.names, args.list.names )
+			names( args.list )[ empty.names ]        <- common.names[ 1 : length( empty.names ) ]
+			frmls[ names( args.list ) ]              <- args.list
+			frmls.chars                              <- as.character( frmls )
+			frmls.chars[ nchar( frmls.chars ) > 15 ] <- paste0( substr( frmls.chars[ nchar( frmls.chars ) > 15 ], 1, 10 ), " ... " )
+			return(
+				list(
+					FUNC = func,
+					ARGS = frmls,
+					CALL = paste0(
+						func,
+						"( ",
+						paste( frmls.names, frmls.chars, sep = "=", collapse = ", " ),
+						ifelse( length( frmls ) < 1, ")", " )" )
+					)
+				)
+			)
+		}
+		list(
+			FUNC = func,
+			ARGS = args.list )
+		#			ARGS = ifelse( is.null( args.list ), list( ), args.list ) )
+	}
+
+#' do.Call
+#'
+#' @name do.Call
+#' @description  calls a function defined by new.Call
+#' @param new.Call a new.Call object
+#' @param verbose.doCall show whats going on beghind the scenes
+#'
+#' @return notting
+#' @export
+#'
+#' @examples
+#' n.C <- new.Call( "print", "Hello World!" )
+#' do.Call( n.C )
+do.Call <-
+	function( new.Call = new.Call( canonical = T ), verbose.doCall = F ) {
+		if( verbose.doCall == T  && ! is.null( new.Call$CALL ) )
+			print( new.Call$CALL )
+		if( is.null( new.Call$ARGS ) )
+			do.call( new.Call$FUNC, list( ) )
+		else
+			do.call( new.Call$FUNC, new.Call$ARGS )
+	}
+
+#' dist.Call
+#'
+#' @name dist.Call
+#' @description  a new.Call object for distribution's use
+#' @param prefix a prefix like "", "d", "p", "q", "r"
+#' @param family a gamlss family or "norm"
+#' @param ...  arguments of the call
+#' @param canonical if TRUE a call with all neccessary arguments is called. Missing will be filled by args of the function's default call.
+#'
+#' @return a new.Call object for calling with do.Call
+#' @export
+#'
+#' @examples
+#' d.C <- dist.Call( "r", "BCPE", 10, canonical = T )
+#' do.Call( d.C, verbose.doCall = T )
+dist.Call <-
+	function( prefix = "p", family = "norm", ... ) {
+		new.Call( paste0( prefix, family ), ... )
+	}
+
+#' gamlss.families
+#'
+#' @name gamlss.families
+#' @description  lists all available gamlss families
+#' @return a vector of gamlss family names
+#' @export
+#'
+#' @examples
+#' gamlss.families( )
+gamlss.families <-
+	function( ) {
+		intersect(
+			intersect(
+				gsub( "^d", "", ls( "package:gamlss.dist" )[ grepl( "^d[A-Z]+", ls( "package:gamlss.dist" ) ) ] ),
+				gsub( "^p", "", ls( "package:gamlss.dist" )[ grepl( "^p[A-Z]+", ls( "package:gamlss.dist" ) ) ] ) ),
+			intersect(
+				gsub( "^q", "", ls( "package:gamlss.dist" )[ grepl( "^q[A-Z]+", ls( "package:gamlss.dist" ) ) ] ),
+				gsub( "^r", "", ls( "package:gamlss.dist" )[ grepl( "^r[A-Z]+", ls( "package:gamlss.dist" ) ) ] ) ) ) }
+
+#' xdist
+#'
+#' @name xdist
+#' @description  call a dist, ddist, pdist, qdist, rdist
+#' @param x prefix "no", "d", "p", "q", "r"
+#' @param family a gamlss family like BCTo
+#' @param ... arguments for the constructed call
+#' @param verbose tell what doing
+#'
+#' @return the expected data
+#' @export
+#'
+#' @examples
+#' xdist( "r", "NO", 10 )
+xdist <-
+	function ( x = "p", family = "NO", ..., verbose = F ) {
+		if( verbose ) {
+			print( paste0( "pdf of ", family, " with arguments:" ) )
+			print( paste0( names( list( ... ) ),": ", list( ... ) ) ) }
+		do.call( eval( parse( text =  paste0( "gamlss.dist::", x, family ) ) ), list( ... ) )
+	}
+
+#' ddist
+#'
+#' @name ddist
+#' @description  call a density function of some gamlss family
+#' @param family a gamlss family like BCTo
+#' @param ... arguments for density function
+#' @param verbose tell what doing
+#'
+#' @return the expected data
+#' @export
+#'
+#' @examples
+#' ddist( "NO", c( -1, 0, +1 ), mu = 0, sigma = 1 )
+ddist <-
+	function ( family = "NO", ..., verbose = F ) {
+		if( verbose ) {
+			print( paste0( "pdf of ", family, " with arguments:" ) )
+			print( paste0( names( list( ... ) ),": ", list( ... ) ) ) }
+		do.call( paste0( "d", family ), list( ... ) )
+	}
+
+#' pdist
+#'
+#' @name pdist
+#' @description  call a pdf of some gamlss family
+#' @param family a gamlss family like BCTo
+#' @param ... arguments for the call
+#' @param verbose tell what doing
+#'
+#' @return the expected data
+#' @export
+#'
+#' @examples
+#' pdist( "NO", c( -1, 0, +1 ), mu = 0, sigma = 1 )
+pdist <-
+	function ( family = "NO", ..., verbose = F ) {
+		if( verbose ) {
+			print( paste0( "cdf of ", family, " with arguments:" ) )
+			print( paste0( names( list( ... ) ),": ", list( ... ) ) ) }
+		do.call( paste0( "p", family ), list( ... ) )
+	}
+
+#' qdist
+#'
+#' @name qdist
+#' @description  call a pdf of some gamlss family
+#' @param family a gamlss family like BCTo
+#' @param ... arguments for the call
+#' @param verbose tell what doing
+#'
+#' @return the expected data
+#' @export
+#'
+#' @examples
+#' qdist( "NO", c( 0.1586553, 0.5000000, 0.8413447 ), mu = 0, sigma = 1 )
+qdist <-
+	function ( family = "NO", ..., verbose = F ) {
+		if( verbose ) {
+			print( paste0( "inv cdf of ", family, " with arguments:" ) )
+			print( paste0( names( list( ... ) ),": ", list( ... ) ) ) }
+		do.call( paste0( "q", family ), list( ... ) )
+	}
+
+#' rdist
+#'
+#' @name rdist
+#' @description  create a random values of some gamlss family
+#' @param family a gamlss family like BCTo
+#' @param ... arguments for the call
+#' @param verbose tell what doing
+#'
+#' @return the expected data
+#' @export
+#'
+#' @examples
+#' rdist( "NO", c( 0.1586553, 0.5, 0.8413447 ), mu = 0, sigma = 1 )
+rdist <-
+	function ( family = "NO", ..., verbose = F ) {
+		if( verbose ) {
+			print( paste0( "random values of ", family, " with arguments:" ) )
+			print( paste0( names( list( ... ) ),": ", list( ... ) ) ) }
+		do.call( paste0( "r", family ), list( ... ) )
 	}
 
 
